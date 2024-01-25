6ae9da1a6b0d723326221743d6843fff176bb720
Bilson Naga <bilsonnaga@Bilsons-MacBook-Pro.local>
2024-01-25 17:30:15 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h1>Memory Game</h1>
+
+        <div>
+          <p>Click on an image to earn points, but don't click on any more than once!</p>
+        </div>
+
+        <div>
+          <p>Score: 0 | Top Score: 0</p>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
